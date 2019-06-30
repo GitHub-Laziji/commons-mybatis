a6a178adeb03be252f9716814935fa426e57f6f8
GitHub-Laziji <c_smtp@163.com>
2019-06-30 17:02:37 +0800

    包名修改

--- src/main/java/org/laziji/commons/mybatis/dao/annotation/Table.java
+++ src/main/java/org/laziji/commons/mybatis/dao/annotation/Table.java
@@ -1,4 +1,4 @@
-package org.laziji.commons.mybatis.dao.annotations;
+package org.laziji.commons.mybatis.dao.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
